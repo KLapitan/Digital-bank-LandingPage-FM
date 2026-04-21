981d100996283e203b50de4275f9d712ecb66d9b
KLapitan <karllapitan5@gmail.com>
2026-04-21 15:50:20 +0800

    added links on navs in sm-lg view

--- src/Components/DBank-NavSection.jsx
+++ src/Components/DBank-NavSection.jsx
@@ -1,22 +1,28 @@
 
 import DBankModal from "./DBank-LinkModal"
 
-
-
 const DBankNav = ({links ,showLinks ,onOpen}) => {
 
 
 return(
 <>
 <nav className="h-auto relative z-50  ">
-  <section className="h-16 border flex flex-row justify-between items-center p-4">
+  <section className="h-16 border flex flex-row justify-between  sm:justify-start sm:gap-6 items-center p-4">
     <picture>
         <img src="src/assets/images/logo-dark.svg" alt="dbank logo" 
           className=""
         />
     </picture>
 
-<button onClick={onOpen}>
+      <ul className="hidden sm:flex flex-row gap-2">
+      {links.map((link) => (
+      <li>{link}</li>
+      ))}
+      
+      </ul>
+
+
+<button onClick={onOpen} className="sm:hidden md:hidden lg:hidden">
     <picture>
         <img src={`${showLinks ? "src/assets/images/icon-close.svg": "src/assets/images/icon-hamburger.svg"}`} alt="hamburger" />
     </picture>
