7a34765aa99440a02a1cc9a5ce509b3ddf192003
KLapitan <karllapitan5@gmail.com>
2026-05-05 18:25:36 +0800

    pulled request from pc

--- src/Components/DBank-IntroSection.jsx
+++ src/Components/DBank-IntroSection.jsx
@@ -2,13 +2,38 @@ const DBankIntro =({showLinks}) => {
 
 return(
 <main className="h-auto ">
+    <section className="h-auto font-PublicSans  ">   
 
-<section>
+    {/* container of hero image and hero detail */}
+          <div className="h-screen  relative overflow-hidden lg:h-155 border lg:bg-NGray100 z-40 ">
+                {/* large view */}
+                  <picture className=" z-40 absolute lg:-top-56 lg:-right-40 lg:scale-85">
+                    <img src="src/assets/images/image-mockups.png" alt="mockup" className="phone" />
+                  </picture>
+
+                {/* small view */}
+                <picture className="lg:hidden z-40 absolute -top-38 ">
+                    <img src="src/assets/images/image-mockups.png" className="w-full h-129" alt="mockup"  />
+                  </picture>
 
 
+                  {/* container of the phone to create the effect of phone passing through the section */}
+              <div className="h-screen flex flex-col lg:flex-row-reverse w-full  bg-NGray50  lg:h-143   lg:overflow-hidden">
+                   {/* bg-image of the phone  */}
+                     <div className="lg:w-3xl h-86 lg:h-screen bg-[url('src/assets/images/bg-intro-mobile.svg')] bg-no-repeat bg-cover lg:bg-[url('src/assets/images/bg-intro-desktop.svg')]  lg:bg-size-[165%] lg:bg-bottom-left  lg:overflow-hidden  border"></div>
 
+                      {/* details  */}
+                      <div className="w-auto h-auto flex flex-col  items-start border gap-4 mt-5 lg:gap-8 lg:justify-center   lg:w-xl lg:mr-2 ">
+                            <h2 className=" text-3xl w-75 border lg:text-5xl text-center font-light tracking-tight text-PBlue950 lg:text-left lg:w-100 ">Next generation digital banking</h2>
 
-</section>
+                            <p className="text-center   text-md p-2 tracking-tight leading-5 text-NGray600 lg:text-left lg:w-95 ">Take your financial life online. Your Digitalbank account will be a one-stop-shop 
+                              for spending, saving, budgeting, investing, and much more.</p>  
+                      </div>
+
+                </div>  
+
+          </div>
+    </section>
 
 </main>
 )}
