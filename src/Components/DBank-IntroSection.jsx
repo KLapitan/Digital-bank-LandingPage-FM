835378654fe045442a5e2af27c61d336729a6205
KLapitan <karllapitan5@gmail.com>
2026-05-11 15:10:02 +0800

    fix:Ui ,added reusable button

--- src/Components/DBank-IntroSection.jsx
+++ src/Components/DBank-IntroSection.jsx
@@ -1,3 +1,4 @@
+import DBankInviteButton from "./DBank-Button";
 const DBankIntro =({showLinks}) => {
 
 return(
@@ -5,34 +6,38 @@ return(
     <section className="h-auto font-PublicSans  ">   
 
     {/* container of hero image and hero detail */}
-          <div className="h-screen  relative overflow-hidden lg:h-155 border lg:bg-NGray100 z-40 ">
+          <div className="h-screen border  relative overflow-hidden lg:h-155 lg:bg-NGray100 z-40 ">
                 {/* large view */}
                   <picture className="hidden lg:block z-50 absolute lg:-top-56 lg:-right-40 lg:scale-85">
                     <img src="src/assets/images/image-mockups.png" alt="mockup" className="phone" />
                   </picture>
 
                 {/* small view */}
-                <picture className="lg:hidden z-40 absolute -top-37  md:left-50 p-2 scale-100">
-                    <img src="src/assets/images/image-mockups.png" className="w-full h-115" alt="mockup"  />
+                <picture className="lg:hidden z-40 absolute -top-37  md:left-32  p-2 scale-100">
+                    <img src="src/assets/images/image-mockups.png" className="w-full md:w-130 h-115" alt="mockup"  />
                   </picture>
 
 
                   {/* container of the phone to create the effect of phone passing through the section */}
-              <div className="h-screen flex flex-col lg:flex-row-reverse w-full  bg-NGray50  lg:h-143   lg:overflow-hidden">
+              <div className="h-screen  border flex flex-col lg:flex-row-reverse md:gap-2   w-full  bg-NGray50  lg:h-143  lg:overflow-hidden">
                    {/* bg-image of the phone  */}
-                     <div className="lg:w-3xl h-80 lg:h-screen bg-size-[100%] bg-center bg-[url('src/assets/images/bg-intro-mobile.svg')] bg-no-repeat md:bg-size-[100%] md:bg-center lg:bg-[url('src/assets/images/bg-intro-desktop.svg')]  lg:bg-size-[145%] lg:bg-bottom-left  lg:overflow-hidden  border"></div>
+                     <div className="lg:w-3xl h-80  lg:h-screen bg-size-[100%] bg-center bg-[url('src/assets/images/bg-intro-mobile.svg')] bg-no-repeat md:bg-size-[100%] md:bg-center lg:bg-[url('src/assets/images/bg-intro-desktop.svg')]  lg:bg-size-[190%] lg:bg-bottom-left  lg:overflow-hidden    "></div>
 
                       {/* details  */}
-                      <div className="w-auto h-auto flex flex-col  items-center  border gap-4 mt-5 lg:gap-8 lg:justify-center lg:items-start   lg:w-xl lg:mr-2 ">
-                            <h2 className=" text-4xl w-90 border lg:text-5xl text-center font-light tracking-tight text-PBlue950 lg:text-left lg:w-100 lg:ml-10 ">Next generation digital banking</h2>
+                      <div className="w-auto h-auto  flex flex-col   items-center gap-4 mt-5  lg:gap-8 lg:justify-center lg:items-start   lg:w-lg   lg:mr-2 ">
+                            <h2 className=" text-4xl w-89  md:text-5xl   lg:text-5xl text-center font-light tracking-tight text-PBlue950 lg:text-left lg:w-100 lg:ml-6 ">Next generation digital banking</h2>
 
-                            <p className="text-center w-90   text-md p-2 tracking-tight leading-5 text-NGray600 md:text-center  md:w-md md:h-32 lg:text-left lg:w-95 lg:ml-10 ">Take your financial life online. Your Digitalbank account will be a one-stop-shop 
+                            <p className="text-center w-90   text-md p-2 tracking-tight leading-5 text-NGray600 md:text-center  md:w-md md:h-32 lg:text-left lg:w-95 lg:ml-6 ">Take your financial life online. Your Digitalbank account will be a one-stop-shop 
                               for spending, saving, budgeting, investing, ands much more.</p>  
+                     <DBankInviteButton className="mt-2 lg:-mt-18 lg:ml-7">Request Invite</DBankInviteButton>
                       </div>
-
+      
                 </div>  
 
           </div>
+
+
+
     </section>
 
 </main>
